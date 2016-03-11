3b192ab2efab94920e0598aa09e61807cae26068
Vanilla <mail@liyaodong.com>
2016-03-11 13:23:09 +0800

    basic str array print

--- example.js
+++ example.js
@@ -3,7 +3,21 @@ import Gugu from './lib/index';
 
 const inst = new Gugu(CONFIG);
 
-inst.setup()
-  .then(() => inst.print('xxx'))
-  .then(() => console.log('print ok'))
-  .catch(inst._catchErr);
+const strs = [
+  {
+    type: 'text',
+    value: 'Hello',
+  },
+  {
+    type: 'text',
+    value: 'world',
+  },
+];
+
+// inst.setup()
+//   .then(() => inst.print(strs))
+//   .then(() => console.log('print ok'))
+//   .catch(inst._catchErr);
+
+
+inst._flatStrArray(strs).catch(inst.catchErr);
--- test/lib/index.js
+++ test/lib/index.js
@@ -1,5 +1,11 @@
-import assert from 'assert';
-// import { expect } from 'chai';
+// import assert from 'assert';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+
+chai.use(chaiAsPromised);
+
+const assert = chai.assert;
+
 
 import CONFIG from '../../config';
 import Gugu from '../../lib/index';
@@ -14,9 +20,21 @@ describe('GUGU sdk', () => {
   });
 
   describe('#print', () => {
-    it('can not pass empty str', () => {
-      assert.ok(typeof test1.print() === 'undefined');
-      assert.ok(typeof test1.print('') === 'undefined');
+    it('should reject when empty str', () => {
+      assert.isRejected(test1.print(''));
+      assert.isRejected(test1.print());
+    });
+
+    const printArr = [
+      { type: 'text', value: 'Test 101' },
+      { type: 'text', value: 'Test 101' },
+      { type: 'text', value: 'Test 101' },
+      // { type: 'pic', value: '../images/npm.png' },
+      // { type: 'pic_url', value: 'https://dn-geekpark-new.qbox.me/uploads/image/file/6b/24/6b24096efce8f2842c5c764f58acfca9.jpg' },
+    ];
+
+    it('#_flatStrArray hould return str join with | and have same length', () => {
+      assert.ok(test1._flatStrArray(printArr).then(d => d.split('|').length === printArr.length));
     });
   });
 });
